ae910062646e4f24c98bcbc19fa5ecac56bb48a1
DominicSamoes <dominicsamo@outlook.com>
2026-03-22 16:40:13 +0200

    Project Inititialization

---
+++ ems-backend/src/test/java/com/umcap/ems_backend/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umcap.ems_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
